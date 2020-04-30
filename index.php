b045d4b08c7db1daf2ea52600e9f61d04792d7c1
hv00266 <hv00266>
2020-04-29 22:08:51 -0400

    Condensed index to one php file

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
    
     if($_SERVER["REQUEST_METHOD"] == "POST") {
       
-        //get email and password from post request
+        //get eid and password from post request
         $eid = mysqli_real_escape_string($db,$_POST['eid']);
         //$password = mysqli_real_escape_string($db,$_POST['password']); 
         
@@ -12,8 +12,8 @@
         $query = "SELECT * FROM Resident WHERE EID = '$eid'/*  and userPassword = '$password' */";
         $result = mysqli_query($db, $query);
         $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        //$fName = $row['fname'];
-        //$eid = $row['EID'];
+        $fName = $row['fname'];
+        $eid = $row['EID'];
       
         $count = mysqli_num_rows($result);
        
@@ -21,7 +21,7 @@
         if($count == 1) 
         {
             //set session variables
-            //$_SESSION['fname'] = $fName;
+            $_SESSION['fname'] = $fName;
             $_SESSION['eid'] = $eid;            
          
             //redirect to dashboard
@@ -36,3 +36,42 @@
         }
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Student Housing Portal</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="/studenthousing/css/index.css" type="text/css" />
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li style="float:left;"> 
+          <a href="#home" style=""><strong>Student Housing Portal</strong></a>
+        </li>
+      </ul>
+    </nav>
+
+    <main>
+      <div id="login-container">
+        <h1>Student Housing Portal <span class="orange"></span></h1>
+        <div class="login">
+            <h2>Login</h2>
+            <form name="login" action="index.php" method="POST">
+                <label for="eid">EID</label>
+                <input type="text" id="eid" name="eid" placeholder="123456789" />
+
+                <!-- <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="*******" /> -->
+
+                <div class="center">
+                    <input type="submit" value="Submit" class="right"/>
+                </div>
+            </form>
+        </div>
+      </div>
+    </main>
+
+  </body>
+</html>
--- userpage.php
+++ userpage.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
 
-    //echo $_SESSION['fname'];
+    echo $_SESSION['fname'];
     echo $_SESSION['eid'];
 ?>
