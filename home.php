8fda39fe451b7c259aedeb139a073a56c2b8b6ac
hv00266 <hv00266>
2020-04-30 22:14:02 -0400

    Added nav bar

--- home.php
+++ home.php
@@ -24,7 +24,7 @@
         <main>
             <div class="card-container">
                 <div class="column">
-                <a class="cardLink" href="rough%20resident%20page.html"><div class="card">
+                <a class="cardLink" href="residentform.html"><div class="card">
                     <div class="container" style="display: inline-block;">
                         <h4><b>Residents</b></h4>
                         <p>Sign up for a dorm</p>
@@ -32,7 +32,7 @@
                 </div></a>
                 </div>
                 <div class="column">
-                <a class="cardLink" href="rough%20llc%20page.php"><div class="card">
+                <a class="cardLink" href="llcpage.html"><div class="card">
                     <div class="container" style="display: inline-block;">
                         <h4><b>Living Learning Centers</b></h4>
                         <p>Learn about our LLCs</p>
@@ -48,7 +48,7 @@
                 </div></a>
                 </div>
                 <div class="column">
-                <a class="cardLink" href="director.php"><div class="card">
+                <a class="cardLink" href="directorpage.html"><div class="card">
                     <div class="container" style="display: inline-block;">
                         <h4><b>Directors</b></h4>
                         <p>Director info</p>
